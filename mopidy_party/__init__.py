b933019e8377b82ee494935d9a4fe84b98673894
RealityFork <mystx@chaostheory.co.nz>
2017-01-02 18:48:04 +1300

    fixed tabs

--- mopidy_party/__init__.py
+++ mopidy_party/__init__.py
@@ -11,61 +11,61 @@ __version__ = '0.1.2'
 
 class PartyRequestHandler(tornado.web.RequestHandler):
 
-	def initialize(self, core, data, config):
-		self.core = core
-		self.data = data
-		self.requiredVotes = config["party"]["votes_to_skip"]
+    def initialize(self, core, data, config):
+        self.core = core
+        self.data = data
+        self.requiredVotes = config["party"]["votes_to_skip"]
 
-	def get(self):
-		currentTrack = self.core.playback.get_current_track().get()
-		if (currentTrack == None): return
-		currentTrackURI = currentTrack.uri
+    def get(self):
+        currentTrack = self.core.playback.get_current_track().get()
+        if (currentTrack == None): return
+        currentTrackURI = currentTrack.uri
 
-		# If the current track is different to the one stored, clear votes
-		if (currentTrackURI != self.data["track"]):
-			self.data["track"] = currentTrackURI
-			self.data["votes"] = []
+        # If the current track is different to the one stored, clear votes
+        if (currentTrackURI != self.data["track"]):
+            self.data["track"] = currentTrackURI
+            self.data["votes"] = []
 
-		if (self.request.remote_ip in self.data["votes"]): # User has already voted
-			self.write("You have already voted to skip this song =)")
-		else: # Valid vote
-			self.data["votes"].append(self.request.remote_ip)
-			if (len(self.data["votes"]) == self.requiredVotes):
-				self.core.playback.next()
-				self.write("Skipping...")
-			else:
-				self.write("You have voted to skip this song. ("+str(self.requiredVotes-len(self.data["votes"]))+" more votes needed)")
+        if (self.request.remote_ip in self.data["votes"]): # User has already voted
+            self.write("You have already voted to skip this song =)")
+        else: # Valid vote
+            self.data["votes"].append(self.request.remote_ip)
+            if (len(self.data["votes"]) == self.requiredVotes):
+                self.core.playback.next()
+                self.write("Skipping...")
+            else:
+                self.write("You have voted to skip this song. ("+str(self.requiredVotes-len(self.data["votes"]))+" more votes needed)")
 
 
 
 def party_factory(config, core):
-	data = {'track':"", 'votes':[]}
-	return [
-	('/vote', PartyRequestHandler, {'core': core, 'data':data, 'config':config})
-	]
+    data = {'track':"", 'votes':[]}
+    return [
+    ('/vote', PartyRequestHandler, {'core': core, 'data':data, 'config':config})
+    ]
 
 
 class Extension(ext.Extension):
 
-	dist_name = 'Mopidy-Party'
-	ext_name = 'party'
-	version = __version__
+    dist_name = 'Mopidy-Party'
+    ext_name = 'party'
+    version = __version__
 
-	def get_default_config(self):
-		conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
-		return config.read(conf_file)
+    def get_default_config(self):
+        conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
+        return config.read(conf_file)
 
-	def get_config_schema(self):
-		schema = super(Extension, self).get_config_schema()
-		schema['votes_to_skip'] = config.Integer(minimum=0)
-		return schema
+    def get_config_schema(self):
+        schema = super(Extension, self).get_config_schema()
+        schema['votes_to_skip'] = config.Integer(minimum=0)
+        return schema
 
-	def setup(self, registry):
-		registry.add('http:static', {
+    def setup(self, registry):
+        registry.add('http:static', {
             'name': self.ext_name,
             'path': os.path.join(os.path.dirname(__file__), 'static'),
         })
-		registry.add('http:app', {
-			'name': self.ext_name,
-			'factory': party_factory,
-		})
+        registry.add('http:app', {
+            'name': self.ext_name,
+            'factory': party_factory,
+        })
