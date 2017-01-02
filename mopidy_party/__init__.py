611227db44388097f9017608f3b5a21ae5a1ce6b
RealityFork <mystx@chaostheory.co.nz>
2017-01-02 17:42:19 +1300

    indentation errors fixed

--- mopidy_party/__init__.py
+++ mopidy_party/__init__.py
@@ -11,62 +11,59 @@ __version__ = '0.1.2'
 
 class PartyRequestHandler(tornado.web.RequestHandler):
 
-    def initialize(self, core, config):
+	def initialize(self, core, config):
 		self.core = core
 		self.data = {'track':"", 'votes':[]}
 		self.requiredVotes = config["party"]["votes_to_skip"]
 
-    def get(self):
+	def get(self):
 		currentTrack = self.core.playback.get_current_track().get()
 		if (currentTrack == None): return
 		currentTrackURI = currentTrack.uri
 
 		# If the current track is different to the one stored, clear votes
 		if (currentTrackURI != self.data["track"]):
-		    self.data["track"] = currentTrackURI
-		    self.data["votes"] = []
+			self.data["track"] = currentTrackURI
+			self.data["votes"] = []
 
 		if (self.request.remote_ip in self.data["votes"]): # User has already voted
-		    self.write("You have already voted to skip this song =)")
+			self.write("You have already voted to skip this song =)")
 		else: # Valid vote
-   	         self.data["votes"].append(self.request.remote_ip)
-	  	  if (len(self.data["votes"]) == self.requiredVotes):
-			self.core.playback.next()
-			self.write("Skipping...")
-		    else:
-		    	self.write("You have voted to skip this song. ("+str(self.requiredVotes-len(self.data["votes"]))+" more votes needed)")
+			self.data["votes"].append(self.request.remote_ip)
+			if (len(self.data["votes"]) == self.requiredVotes):
+				self.core.playback.next()
+				self.write("Skipping...")
+			else:
+				self.write("You have voted to skip this song. ("+str(self.requiredVotes-len(self.data["votes"]))+" more votes needed)")
 
 
 
 def party_factory(config, core):
-	global REQUIRED_VOTES
-	REQUIRED_VOT
-    data = {'track': "", 'votes': []}
-    return [
+	return [
 	('/vote', PartyRequestHandler, {'core': core, 'data':data, 'config':config})
-    ]
+	]
 
 
 class Extension(ext.Extension):
 
-    dist_name = 'Mopidy-Party'
-    ext_name = 'party'
-    version = __version__
+	dist_name = 'Mopidy-Party'
+	ext_name = 'party'
+	version = __version__
 
-    def get_default_config(self):
-        conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
-        return config.read(conf_file)
+	def get_default_config(self):
+		conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
+		return config.read(conf_file)
 
-    def get_config_schema(self):
-        schema = super(Extension, self).get_config_schema()
-        return schema
+	def get_config_schema(self):
+		schema = super(Extension, self).get_config_schema()
+		return schema
 
-    def setup(self, registry):
-        registry.add('http:static', {
+	def setup(self, registry):
+		registry.add('http:static', {
             'name': self.ext_name,
             'path': os.path.join(os.path.dirname(__file__), 'static'),
         })
 		registry.add('http:app', {
-	    	'name': self.ext_name,
-	    	'factory': party_factory,
+			'name': self.ext_name,
+			'factory': party_factory,
 		})
